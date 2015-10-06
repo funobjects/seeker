870d77d9532d511f8e6ab4334b04f5ac0a9f4682
Robert Fries <robert@robertfries.com>
2015-10-05 19:06:00 -0700

    Cleanup; placeholder for Bonjour

--- src/main/scala/org/funobjects/seeker/UpnpSeeker.scala
+++ src/main/scala/org/funobjects/seeker/UpnpSeeker.scala
@@ -17,44 +17,26 @@
 package org.funobjects.seeker
 
 import java.net.InetAddress
+import java.util.concurrent.TimeUnit
 
 import org.cybergarage.upnp.{Service => UpnpService, Device => UpnpDevice, ControlPoint}
 import org.cybergarage.upnp.device.DeviceChangeListener
 
 import scala.collection.JavaConverters._
+import scala.concurrent.duration.Duration
 
 /**
  * Find various things via UPnP
  */
-class UpnpSeeker {
+class UpnpSeeker extends Seeker {
 
   import Seeker._
 
-  def seek(seconds: Int): Map[Device,InetAddress] = {
-    class Listener extends ControlPoint with DeviceChangeListener {
-
-      //addDeviceChangeListener(this)
-
-      override def deviceAdded(device: UpnpDevice): Unit =
-        println(
-          s"""dev add
-              |  udn ${device.getUDN} ${device.getUUID}
-              |  manufacturer ${device.getManufacture}
-              |  model name ${device.getModelName}
-              |  friendly name ${device.getFriendlyName}
-              |  desc ${Option(device.getModelDescription).getOrElse("n/a")}
-              |  dev type ${device.getDeviceType}
-              |  remote addr ${device.getSSDPPacket.getRemoteInetAddress}
-              |  server ${device.getSSDPPacket.getServer}
-              |  services: ${device.getServiceList.asScala.map(_.asInstanceOf[UpnpService]).map(svc => svc.getServiceType + " " + svc.getServiceID).mkString(",")}
-              |  """.stripMargin)
-
-      override def deviceRemoved(device: UpnpDevice): Unit = println("dev rm")
-    }
-    val seeker = new Listener
+  override def seek(duration: Duration): Map[Device,InetAddress] = {
+    val seeker = new ControlPoint()
     seeker.start()
     seeker.search()
-    Thread.sleep(seconds * 1000)
+    Thread.sleep(duration.toMillis)
     seeker.stop()
     seeker.getDeviceList
       .asScala
@@ -70,7 +52,7 @@ class UpnpSeeker {
 object UpnpSeeker {
   def main(args: Array[String]): Unit = {
     val seeker = new UpnpSeeker()
-    val devs = seeker.seek(5)
+    val devs = seeker.seek(Duration(5, TimeUnit.SECONDS))
     println(devs.mkString("\n"))
   }
 }
